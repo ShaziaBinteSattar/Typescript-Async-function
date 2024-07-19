da8e6facfdd0ff05ece292c4cf0a5f108f0d1012
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-19 16:24:27 +0500

    Add files via upload

---
+++ Practise_FolderQ2/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Question 2: Create a TypeScript function that takes a callback function as an argument and uses setTimeout to call the callback after 1 second.
+//I am trying to create a TypeScript function that takes a callback function as an argument and uses setTimeout to call the callback after 1 second. Here's the code I have so far:
+function delay(callback) {
+    setTimeout(callback, 1000);
+}
+function myCallback() {
+    console.log("Callback executed after 1 second");
+}
+delay(myCallback);
