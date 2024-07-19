04ead5145e98a5ed48abfb44a8700bc431a7b572
Shazia Binte Abdus Sattar <159663846+ShaziaBinteSattar@users.noreply.github.com>
2024-07-19 16:26:12 +0500

    Add files via upload

---
+++ Practise_FolderQ3/main.js
@@ -0,0 +1,47 @@
+"use strict";
+//Question 3: Write a TypeScript function that returns a Promise that resolves with the value "Resolved!"
+//after 3 seconds. 
+function delayedResolve() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Resolved! in 3 seconds");
+        }, 3000);
+    });
+}
+delayedResolve().then((result) => {
+    // This will log "Resolved!" to the console after 3 seconds
+    console.log(result);
+});
+function typeScript() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(" I have diligently worked on learning and practicing to overcome my weakness in  typescript to write coding is :\n Resolved! in 5 seconds");
+        }, 5000);
+    });
+}
+typeScript().then((result) => {
+    // This will log "Resolved!" to the console after 3 seconds
+    console.log(result);
+});
+function ItExam(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (timeout < 1000) {
+                reject("Promise not resolved within specified time in 2 seconds.");
+            }
+            else {
+                resolve("Resolved! if the Promise is resolved within 1 seconds");
+            }
+        }, timeout);
+    });
+}
+// We are passing 4000 milliseconds as the timeout 
+ItExam(4000)
+    .then((result1) => {
+    // This will log "Resolved!" if the Promise is resolved within 1 seconds
+    console.log(result1);
+})
+    .catch((error1) => {
+    // This will log the error!! message if the Promise is not resolved within 3 seconds 
+    console.log(error1);
+});
